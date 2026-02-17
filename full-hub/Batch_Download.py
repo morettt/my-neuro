e9cd3d11de258eb19006a83120aee91d7dd8df41
xxxiu <129813663+morettt@users.noreply.github.com>
2026-02-18 04:42:42 +0800

    Update Batch_Download.py

--- full-hub/Batch_Download.py
+++ full-hub/Batch_Download.py
@@ -15,7 +15,7 @@ import subprocess
 system = platform.system()
 
 # live-2d版本号-
-version_tag = "v6.1.7"
+version_tag = "v6.1.8"
 
 # 禁用SSL警告
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
